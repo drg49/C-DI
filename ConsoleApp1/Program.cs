e191d50ea0cab50d89cb05ffc2be26d53da271e2
Danny Gavin <homevpn14@gmail.com>
2021-10-31 18:03:29 -0400

    First commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace PalindromeAlgorithm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a word to see if it is a palindrome: ");
+            string word = Console.ReadLine();
+
+
+            static void isPalindrome(string input)
+            {
+                char[] charArray = input.ToCharArray();
+                Array.Reverse(charArray);                
+                string reversedWord = new string(charArray);
+                if (input == reversedWord)
+                {
+                    Console.WriteLine($"{input} is a palindrome");
+                } 
+                else
+                {
+                    Console.WriteLine($"{input} is not a palindrome");
+                }
+            }
+
+            isPalindrome(word);
+        }
+    }
+}
---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace DependencyInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BusinessLogicService myObj = new BusinessLogicService(new StudentService());
+            Console.ReadKey();
+        }
+    }
+
+    public class BusinessLogicService
+    {
+        private IService iService;
+
+        public BusinessLogicService(IService _iService)
+        {
+            this.iService = _iService;
+            this.iService.GetFirstName();
+            this.iService.GetLastName();
+        }
+    }
+
+    public interface IService
+    {
+        void GetFirstName();
+        void GetLastName();
+    }
+
+    public class StudentService : IService
+    {
+        public void GetFirstName()
+        {
+            Console.WriteLine("Student First Name");
+        }
+        public void GetLastName()
+        {
+            Console.WriteLine("Student Last Name");
+        }
+    }
+
+    public class TeacherService : IService
+    {
+        public void GetFirstName()
+        {
+            Console.WriteLine("Teacher First Name");
+        }
+
+        public void GetLastName()
+        {
+            Console.WriteLine("Teacher Last Name");
+        }
+    }
+
+}
+
+
+
+
+
+
+// The issue here is that the student class is dependent on the DataService class.
+// Dependency Injection can be used to fix this issue.
+public class Student
+{
+    DataService obj = new DataService();
+
+    public void Get()
+    {
+        obj.GetName();
+    }
+}
+
+public class DataService
+{
+    public void GetName()
+    {
+
+    }
+}
